19f60570d5c02f038c296bc82b0b59d82aa6d2a6
Suraj P Acharya <118610857+suraj-p-acharya@users.noreply.github.com>
2024-03-03 04:04:35 +0530

    Update Reverse_Array.py

--- Python/Programs/Arrays/Reverse_Array.py
+++ Python/Programs/Arrays/Reverse_Array.py
@@ -25,7 +25,7 @@ size = int(input())
 arr=[]
 print("Enter elements of array:", end = " ")
 
-#Lop to get all the elements of array from user
+#Loop to get all the elements of array from user
 for i in range(size):
     arr.append(int(input()))
 
